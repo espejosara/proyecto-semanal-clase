11d8fa7f406ef43497f22b9347835c4951ab4de9
espejosara <espejo.sara93@gmail.com>
2026-04-05 15:01:40 +0200

    js/templates/template.js

---
+++ js/templates/template.js
@@ -0,0 +1,33 @@
+
+
+export const header = `
+  <nav class="nav-container">
+    
+    <!-- LOGO -->
+    <a class="logo" href="index.html"><strong>SE</strong></a>
+
+    <!-- HAMBURGUESA -->
+    <div class="hamburger" id="hamburger">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+
+    <!-- NAV -->
+    <ul class="nav-list" id="nav-links">
+      <li><a href="#about">About</a></li>
+      <li><a href="#skills">Skills</a></li>
+      <li><a href="#cheatsheets">Cheatsheets</a></li>
+      <li><a href="#recursos">Recursos</a></li>
+      <li><a href="contacto.html">Contacto</a></li>
+
+      <!-- DARK MODE -->
+      <li>
+        <button id="dark-mode-toggle" class="dark-mode-btn">
+          <i data-lucide="moon"></i>
+        </button>
+      </li>
+    </ul>
+
+  </nav>
+`;
